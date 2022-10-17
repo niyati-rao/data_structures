3a8ba32fb2d60f81fc8c32a0c3d6c131cc1c5911
unknown <niyati.rao_ug24@ashoka.edu.in>
2022-10-18 02:56:24 +0530

    initial commit

---
+++ linkedlists.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node *next;
+};                          //remember to put semicolon
+
+void printlist(struct node *list)
+{ while (list!=NULL)
+
+}
+
+int main()
+{
+
+}
+
