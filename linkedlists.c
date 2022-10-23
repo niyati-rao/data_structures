fb5fe3a229a4230f509e57329eefa548790f3dfb
unknown <niyati.rao_ug24@ashoka.edu.in>
2022-10-24 04:49:46 +0530

    mostly done, need to figure functions

--- linkedlists.c
+++ linkedlists.c
@@ -4,15 +4,38 @@
 struct node {
     int value;
     struct node *next;
-};                          //remember to put semicolon
-
-void printlist(struct node *list)
-{ while (list!=NULL)
-
-}
-
+}; 
 int main()
 {
+struct node *prev,*head,*list;
+int n;
+    printf ("Enter number of elements:");
+    scanf("%d",&n);// & means that its an address
+    head=NULL; //head is null means no linked list exists
+    printf("enter values:");
+    for(int i=0;i<n;i++)
+    {
+        list=malloc(sizeof(struct node));// alloting space one node at a time
+        scanf("%d",&list->value);
+        list->next=NULL;
+        if(head==NULL){
+            head=list;//marking the head
+        }
+        else{
+            prev->next=list;//
+        }
+        prev=list;// marking current node
+    }
+    list=head;
+    while (list!=NULL)
+    {
+        printf("%d\n",list->value);
+        list=list->next;
+    }
 
+    return 0;
 }
+
+
+
 
