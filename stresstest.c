04e26ef1d8117a2a5d58895e66f64285d23ec76a
niyati-rao <92772812+niyati-rao@users.noreply.github.com>
2022-11-08 17:54:49 +0530

    Add files via upload
    checking when floating point doesn't add well

---
+++ stresstest.c
@@ -0,0 +1,15 @@
+//stresstesting loops for floating point as numbers are stored as approximate
+//decimal to binary lag
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float i=0.00000001;
+    float m=0.0000001; //the max val to be reached
+    int count=0;
+
+    for(float sum=0.0;sum<m;sum+=i)
+        {
+            printf("%d additions done\n",count);
+            count++;}
+}
