4d333f34d91590c1a4a4e341f105f5abebba1f2a
niyati-rao <92772812+niyati-rao@users.noreply.github.com>
2022-11-13 12:37:49 +0530

    ++using struct

--- pointerplay.c
+++ pointerplay.c
@@ -7,12 +7,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 //----------------------------------POINTER PLAY-------------------------------
+struct ptr{
+    char *point;// creating alternate structures
+};
+
 int main()
 {
 //------------------------ * MEANS VALUE & MEANS ADDRESS ----------------------
     char ch='c';
     printf("constant is %c \n", ch );
     printf("address is  %p\n", &ch);// remember %p is to be used
+//------------------------ USING NEWLY CREATED STRUCTURE  ----------------------
+    struct ptr pointer;
+    pointer.point = &ch;
+    printf("using pointer is %c \n", *pointer.point);
 //-----------------------IF TYPE IS * MEANS ADDRESS ---------------------------
     char *c= &ch;
     printf("constant is %c \n", *c );
